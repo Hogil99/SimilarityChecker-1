2613a9ba95147b5c4b4b45165f7ca56911b2eca5
Hogil99 <hogil.gwak@gmail.com>
2023-06-02 11:48:41 +0900

    add SimilarityCheker fixture

--- Project1/Sample-Test1/test.cpp
+++ Project1/Sample-Test1/test.cpp
@@ -1,12 +1,16 @@
 #include "pch.h"
 #include "../Project1/SimilarityChecker.cpp"
 
-TEST(LengthCheckTest, length1) {
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
 	SimilarityChecker similarityChecker;
+}; 
+
+TEST_F(SimilarityCheckerFixture, length1) {
 	EXPECT_THROW(similarityChecker.checkLentghPoint("", ""), std::invalid_argument);
 }
 
-TEST(LengthCheckTest, length2) {
-	SimilarityChecker similarityChecker;
+TEST_F(SimilarityCheckerFixture, length2) {
 	EXPECT_EQ(60, similarityChecker.checkLentghPoint("ABC", "ABC"));
 }
